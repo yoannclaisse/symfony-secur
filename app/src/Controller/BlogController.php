f1543f4e86fec2fc83e2c3817005d48e9c0e3d4b
yoannclaisse <claisse.yoann@gmail.com>
2023-01-06 13:51:53 +0100

    edit-blog-inside-BlogController

--- app/src/Controller/BlogController.php
+++ app/src/Controller/BlogController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Blog;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Bic;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
@@ -14,5 +19,40 @@ class BlogController extends AbstractController
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
+    }
+
+    #[Route('/blog/edit/{id}', name: 'edit_blog')]
+    // VERIFIE SI LE USER EST CONNECTER
+    #[IsGranted('ROLE_USER')]
+    public function edit(Blog $blog): Response
+    {
+        $user = $this->getUser();
+
+        if ($blog->getAuthor() === $user) {
+            dump('edition ok');
+        } else {
+            dump('edit koooo');
+        }
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test(EntityManagerInterface $em, UserRepository $userRepo): Response
+    {
+        $user = $userRepo->find(4);
+
+        $blog = new Blog();
+        $blog->setContent('123');
+
+        $blog->setAuthor($user);
+
+        $em->persist($blog);
+        $em->flush();
+
+
+        return $this->redirectToRoute('home');
     }
 }
